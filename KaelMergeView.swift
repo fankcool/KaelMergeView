e67c37e447d64f2b497b8336e761baa2e6b70e15
fank <locke_fank@126.com>
2018-12-14 16:48:06 +0800

    all fils

---
+++ KaelMergeView.swift
@@ -0,0 +1,30 @@
+//
+//  KaelMergeView.swift
+//  KaelMergeView
+//
+//  Created by fank on 2018/12/14.
+//  Copyright © 2018年 fank. All rights reserved.
+//
+
+import UIKit
+
+class KaelMergeView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let test = UIView()
+        test.frame = CGRect(x: 20, y: 20, width: 100, height: 100)
+        test.backgroundColor = UIColor.red
+        self.addSubview(test)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    class func kaelMergeView() -> KaelMergeView {
+        return UINib(nibName: "KaelMergeView", bundle: nil).instantiate(withOwner: nil, options: nil).first as! KaelMergeView 
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# KaelMergeView
+
+Private cocoapods test project.
