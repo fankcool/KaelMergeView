742b0cbd454928836072d5247920d1f937e2c2fc
fank <locke_fank@126.com>
2018-12-16 01:15:19 +0800

    Init all fils

--- KaelMergeView.swift
+++ KaelMergeView.swift
@@ -20,7 +20,7 @@ class KaelMergeView: UIView {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     class func kaelMergeView() -> KaelMergeView {
